bc8797c58bc73c354a94f77207ed4d303acc1809
Mikhail <mikerar329@gmail.com>
2019-03-06 23:39:57 +0300

    added file input

--- src/com/sapozhnikov/taxistation/domain/Vehicle.java
+++ src/com/sapozhnikov/taxistation/domain/Vehicle.java
@@ -1,6 +1,12 @@
 package com.sapozhnikov.taxistation.domain;
 
+import com.sapozhnikov.taxistation.domain.impl.Car;
+import com.sapozhnikov.taxistation.domain.impl.Truck;
+import com.sapozhnikov.taxistation.domain.impl.Van;
+
 import java.io.Serializable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 abstract public class Vehicle implements Serializable {
     private Manufacturer manufacturer;
@@ -10,7 +16,7 @@ abstract public class Vehicle implements Serializable {
     private Integer mileage;
     private Double fuelConsumption; // liters per 100km
 
-    public Vehicle() {
+    protected Vehicle() {
     }
 
     public Vehicle(Manufacturer manufacturer,
@@ -71,6 +77,105 @@ abstract public class Vehicle implements Serializable {
 
     public void setFuelConsumption(Double fuelConsumption) {
         this.fuelConsumption = fuelConsumption;
+    }
+
+    public static Vehicle parseVehicle(String string) {
+        Vehicle vehicle = null;
+        Pattern manufacturer = Pattern.compile("manufacturer = ([^,]*|$)");
+        Matcher manufacturerMatcher = manufacturer.matcher(string);
+        boolean manufacturerFound = manufacturerMatcher.find();
+        Pattern model = Pattern.compile("model = ([^,]*|$)");
+        Matcher modelMatcher = model.matcher(string);
+        boolean modelFound = modelMatcher.find();
+        Pattern year = Pattern.compile("year = ([^,]*|$)");
+        Matcher yearMatcher = year.matcher(string);
+        boolean yearFound = yearMatcher.find();
+        Pattern price = Pattern.compile("price = ([^,]*|$)");
+        Matcher priceMatcher = price.matcher(string);
+        boolean priceFound = priceMatcher.find();
+        Pattern mileage = Pattern.compile("mileage = ([^,]*|$)");
+        Matcher mileageMatcher = mileage.matcher(string);
+        boolean mileageFound = mileageMatcher.find();
+        Pattern fuelConsumption = Pattern.compile("fuelConsumption = ([^,]*|$)");
+        Matcher fuelConsumptionMatcher = fuelConsumption.matcher(string);
+        boolean fuelConsumptionFound = fuelConsumptionMatcher.find();
+        Pattern numberOfSeats = Pattern.compile("numberOfSeats = ([^,]*|$)");
+        Matcher numberOfSeatsMatcher = numberOfSeats.matcher(string);
+        boolean numberOfSeatsFound = numberOfSeatsMatcher.find();
+        Pattern cargoPlaces = Pattern.compile("cargoPlaces = ([^,]*|$)");
+        Matcher cargoPlacesMatcher = cargoPlaces.matcher(string);
+        boolean cargoPlacesFound = cargoPlacesMatcher.find();
+        Pattern currentPassengersCount = Pattern.compile("currentPassengersCount = ([^,]*|$)");
+        Matcher currentPassengersCountMatcher = currentPassengersCount.matcher(string);
+        boolean currentPassengersCountFound = currentPassengersCountMatcher.find();
+        Pattern currentCargoCount = Pattern.compile("currentCargoCount = ([^,]*|$)");
+        Matcher currentCargoCountMatcher = currentCargoCount.matcher(string);
+        boolean currentCargoCountFound = currentCargoCountMatcher.find();
+
+
+
+//        System.out.println(manufacturerMatcher.find() );
+//        System.out.println(modelMatcher.find());
+//        System.out.println(yearMatcher.find());
+//        System.out.println(priceMatcher.find());
+//        System.out.println(mileageMatcher.find());
+//        System.out.println(fuelConsumptionMatcher.find());
+        if (manufacturerFound &&
+                modelFound &&
+                yearFound &&
+                priceFound &&
+                mileageFound &&
+                fuelConsumptionFound) {
+            if (numberOfSeatsFound &&
+                    currentPassengersCountFound &&
+                    cargoPlacesFound &&
+                    currentCargoCountFound) {
+                vehicle = new Van(
+                        Manufacturer.valueOf(manufacturerMatcher.group(1).toUpperCase()),
+                        Model.valueOf(modelMatcher.group(1).toUpperCase()),
+                        Integer.valueOf(yearMatcher.group(1)),
+                        Integer.valueOf(priceMatcher.group(1)),
+                        Integer.valueOf(mileageMatcher.group(1)),
+                        Double.valueOf(fuelConsumptionMatcher.group(1)),
+                        Integer.valueOf(numberOfSeatsMatcher.group(1)),
+                        Integer.valueOf(cargoPlacesMatcher.group(1)),
+                        Integer.valueOf(currentPassengersCountMatcher.group(1)),
+                        Integer.valueOf(currentCargoCountMatcher.group(1))
+                );
+
+            } else if (numberOfSeatsFound &&
+                    currentPassengersCountFound) {
+                vehicle = new Car(
+                        Manufacturer.valueOf(manufacturerMatcher.group(1).toUpperCase()),
+                        Model.valueOf(modelMatcher.group(1).toUpperCase()),
+                        Integer.valueOf(yearMatcher.group(1)),
+                        Integer.valueOf(priceMatcher.group(1)),
+                        Integer.valueOf(mileageMatcher.group(1)),
+                        Double.valueOf(fuelConsumptionMatcher.group(1)),
+                        Integer.valueOf(numberOfSeatsMatcher.group(1)),
+                        Integer.valueOf(currentPassengersCountMatcher.group(1))
+                );
+            } else if (cargoPlacesFound &&
+                    currentCargoCountFound) {
+                vehicle = new Truck(
+                        Manufacturer.valueOf(manufacturerMatcher.group(1).toUpperCase()),
+                        Model.valueOf(modelMatcher.group(1).toUpperCase()),
+                        Integer.valueOf(yearMatcher.group(1)),
+                        Integer.valueOf(priceMatcher.group(1)),
+                        Integer.valueOf(mileageMatcher.group(1)),
+                        Double.valueOf(fuelConsumptionMatcher.group(1)),
+                        Integer.valueOf(cargoPlacesMatcher.group(1)),
+                        Integer.valueOf(currentCargoCountMatcher.group(1))
+                );
+            } else {
+                System.out.println("Modern problems require modern solutions!");
+            }
+
+        } else {
+            System.out.println("Incorrect file input");
+        }
+
+        return vehicle;
     }
 
     @Override
--- src/com/sapozhnikov/taxistation/main/Main.java
+++ src/com/sapozhnikov/taxistation/main/Main.java
@@ -10,17 +10,15 @@ import com.sapozhnikov.taxistation.service.VehicleByFuelConsumption;
 import com.sapozhnikov.taxistation.service.VehicleSearch;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Double totalCost = 0.0;
         List<Vehicle> vehicles = new LinkedList<>();
-        File file = new File("file.txt");
-        List<String> strings = new ArrayList<String>();
+        File file = new File("D:\\Java_Labs\\Epam_Homework\\src\\com\\sapozhnikov\\taxistation\\service\\Obj.txt");
         Scanner sc;
 
         try {
@@ -29,64 +27,66 @@ public class Main {
 
             while (sc.hasNextLine()) {
                 String line = sc.nextLine().trim();
-                //.trim() осуществляет обрезание пробелов
-                strings.add(line);
-                System.out.println(line); //печать строки в стандартный вывод
+                vehicles.add(Vehicle.parseVehicle(line));
+
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        vehicles.add(new Car(
-                Manufacturer.BMW,
-                Model.X5,
-                2005,
-                14000,
-                248000,
-                18.2,
-                5,
-                1));
-        System.out.println(vehicles.get(0));
-//        FileOutputStream fileOut = new FileOutputStream("D:/Java_Labs/Epam_Homework/src/com/sapozhnikov/taxistation/service/Obj.txt");
-        vehicles.add(new Car(
-                Manufacturer.LADA,
-                Model.KALINA,
-                2002,
-                3500,
-                748000,
-                7.4,
-                5,
-                2));
-        vehicles.add(new Van(
-                Manufacturer.VOLKSWAGEN,
-                Model.TRANSPORTER,
-                2008,
-                8900,
-                348000,
-                6.5,
-                9,
-                4,
-                6,
-                3));
-        vehicles.add(new Truck(
-                Manufacturer.IVECO,
-                Model.MASSIF,
-                2006,
-                12500,
-                112000,
-                13.4,
-                6,
-                3));
+
+
+//        vehicles.add(new Car(
+//                Manufacturer.BMW,
+//                Model.X5,
+//                2005,
+//                14000,
+//                248000,
+//                18.2,
+//                5,
+//                1));
+//        System.out.println(vehicles.get(0));
+////        FileOutputStream fileOut = new FileOutputStream("D:/Java_Labs/Epam_Homework/src/com/sapozhnikov/taxistation/service/Obj.txt");
+//        vehicles.add(new Car(
+//                Manufacturer.LADA,
+//                Model.KALINA,
+//                2002,
+//                3500,
+//                748000,
+//                7.4,
+//                5,
+//                2));
+//        vehicles.add(new Van(
+//                Manufacturer.VOLKSWAGEN,
+//                Model.TRANSPORTER,
+//                2008,
+//                8900,
+//                348000,
+//                6.5,
+//                9,
+//                4,
+//                6,
+//                3));
+//        vehicles.add(new Truck(
+//                Manufacturer.IVECO,
+//                Model.MASSIF,
+//                2006,
+//                12500,
+//                112000,
+//                13.4,
+//                6,
+//                3));
         for (Vehicle v : vehicles) {
             System.out.println(v.toString());
             totalCost += v.getPrice();
         }
+        System.out.println(" Total cost is: " + totalCost + "\n");
         System.out.println(" Sorted by Fuel Consumption:");
         vehicles.sort(new VehicleByFuelConsumption());
         for (Vehicle v : vehicles) {
             System.out.println(v.toString());
         }
-        System.out.println(" Total cost is: " + totalCost);
+        System.out.println("\nSorted by parameters range:");
         VehicleSearch.searchByYear(2009, 2004, vehicles);
         VehicleSearch.searchByMileage(200000, 350000, vehicles);
 
